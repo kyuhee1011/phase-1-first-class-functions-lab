232a5738469164dfcc64ed5a1faa64b7e1dfefee
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-03-22 21:51:55 -0400

    funciton lab complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (arrayOfDrivers) {
+    return arrayOfDrivers.slice (0,2)
+}
+
+
+const returnLastTwoDrivers = function (arrayOfDrivers) {
+    return arrayOfDrivers.slice (-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(anInteger) {
+    return function (fare) {
+        return fare *anInteger
+    }
+}
+
+const fareDoubler =createFareMultiplier(2);
+
+const fareTripler =createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, func) {
+     return func (arrayOfDrivers)
+}  
